f0d26e381abd03d92e00985a1cc0909324f37f4b
Roberto Ehlert <roberto.ehlert@hotmail.com>
2020-04-09 20:37:52 -0300

    Views Esboçadas

--- IHome/IHome/ConsumoEnergia.xaml.cs
+++ IHome/IHome/ConsumoEnergia.xaml.cs
@@ -25,32 +25,32 @@ namespace IHome
         {
             var entries = new[]
             {
-                new ChartEntry(33.4f)
+                new ChartEntry(1.8f)
                 {
-                    Label = "Switch",
-                    ValueLabel = "33.4M",
+                    Label = "Lampada Quarto",
+                    ValueLabel = "1.8 kWh",
                     Color = SKColor.Parse("#E52510"),
                     TextColor = TextColor
                 },
-                new ChartEntry(95.1f)
+                new ChartEntry(1.1f)
                 {
-                    Label = "PS4",
-                    ValueLabel = "95.1M",
+                    Label = "Lampada Banheiro",
+                    ValueLabel = "1.1 kWh",
                     Color = SKColor.Parse("#003791"),
-                      TextColor = TextColor
+                    TextColor = TextColor
                 },
-                new ChartEntry(42.3f)
+                new ChartEntry(1.3f)
                 {
-                    Label = "Xbox One",
-                    ValueLabel = "42.3M",
+                    Label = "Lampada Cozinha",
+                    ValueLabel = "1.3 kWh",
                     Color = SKColor.Parse("#107b10"),
                       TextColor = TextColor
                 },
-                new ChartEntry(74.4f)
+                new ChartEntry(1.7f)
                 {
-                    Label = "3DS",
-                    ValueLabel = "74.4M",
-                    Color = SKColor.Parse("#E52510"),
+                    Label = "Lampada Quarto 2",
+                    ValueLabel = "1.7 kWh",
+                    Color = SKColor.Parse("#FFFF00"),
                     TextColor = TextColor
                 }
             };
@@ -62,44 +62,44 @@ namespace IHome
                   Entries = entries ,
                   LabelTextSize = 35
                 },
-                new PointChart()
-                 {
-                  Entries = entries ,
-                  LabelTextSize = 35
-                  },
-                new LineChart()
-                {
-                    Entries = entries,
-                    LineMode = LineMode.Straight,
-                    LineSize = 8,
-                    PointMode = PointMode.Square,
-                    PointSize = 18,
-                    LabelTextSize = 35
-                },
-                new DonutChart()
-                { Entries = entries,
-                  LabelTextSize = 35
-                },
-                new RadialGaugeChart()
-                { Entries = entries ,
-                  LabelTextSize = 35
-                },
-                new RadarChart()
-                {
-                  Entries = entries ,
-                  LabelTextSize = 35
-                },
+                //new PointChart()
+                // {
+                //  Entries = entries ,
+                //  LabelTextSize = 35
+                //  },
+                //new LineChart()
+                //{
+                //    Entries = entries,
+                //    LineMode = LineMode.Straight,
+                //    LineSize = 8,
+                //    PointMode = PointMode.Square,
+                //    PointSize = 18,
+                //    LabelTextSize = 35
+                //},
+                //new DonutChart()
+                //{ Entries = entries,
+                //  LabelTextSize = 35
+                //},
+                //new RadialGaugeChart()
+                //{ Entries = entries ,
+                //  LabelTextSize = 35
+                //},
+                //new RadarChart()
+                //{
+                //  Entries = entries ,
+                //  LabelTextSize = 35
+                //},
             };
         }
         protected override void OnAppearing()
         {
             var charts = CreateXamarinSample();
             this.chart1.Chart = charts[0];
-            this.chart2.Chart = charts[1];
-            this.chart3.Chart = charts[2];
-            this.chart4.Chart = charts[3];
-            this.chart5.Chart = charts[4];
-            this.chart6.Chart = charts[5];
+            //this.chart2.Chart = charts[1];
+            //this.chart3.Chart = charts[2];
+            //this.chart4.Chart = charts[3];
+            //this.chart5.Chart = charts[4];
+            //this.chart6.Chart = charts[5];
         }
     }
 }
